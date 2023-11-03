4e3f5159ff22b6a7f98b82080e3e73f378252266
Pradip Ambadas Gangawane <pradip.10729794@ltimindtree.com>
2023-11-03 19:24:17 +0530

    added incremental docker tag version

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export VERSION=v1.0.0
+NEW_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
+export VERSION=$NEW_VERSION
+sed -i '/export VERSION=v1.0.0/d' version.sh

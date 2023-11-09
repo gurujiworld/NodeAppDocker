2165cb7de6a77d78e42ef15b7081da129aae1a4a
Pradip Ambadas Gangawane <pradip.10729794@ltimindtree.com>
2023-11-10 02:27:34 +0530

    added script

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-NEW_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
-export VERSION=$NEW_VERSION
+NEW_VERSION=$(cat version.txt | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
+echo $NEW_VERSION > version.txt
